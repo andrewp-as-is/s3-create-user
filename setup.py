a30cb560deeaf95ea6c3d18ca80d34c73761dc46
andrewp-as-is <russianidiot.github@gmail.com>
2020-06-30 23:04:08 +0300

    init

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python
-from setuptools import setup
+import setuptools
 
-setup()
+setuptools.setup(
+    name='s3-create-user',
+    install_requires=open('requirements.txt').read().splitlines(),
+    packages=setuptools.find_packages(),
+    scripts=['scripts/s3-create-full-access-user','scripts/s3-create-read-only-user']
+)
